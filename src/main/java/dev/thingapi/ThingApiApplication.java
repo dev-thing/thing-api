caf8e4741e10c65298349ccfee943cda41168338
parksangwon <ourtree4@gmail.com>
2019-02-26 23:33:03 +0900

    Initial commit

---
+++ src/main/java/dev/thingapi/ThingApiApplication.java
@@ -0,0 +1,13 @@
+package dev.thingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/thingapi/ThingApiApplicationTests.java
@@ -0,0 +1,16 @@
+package dev.thingapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ThingApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
